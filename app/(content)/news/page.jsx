ab9bbf9368ce6405ef1b160bbbe6b47b9f5ec492
MohammedOsamma <mo856915i@gmail.com>
2024-10-30 09:02:56 +0200

    Showing a 'Loading' Fallback

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -2,7 +2,7 @@ import NewsList from "@/components/news-list";
 import { getAllNews } from "@/lib/news";
 
 const NewsPage = async () => {
-  const news = getAllNews();
+  const news = await getAllNews();
   return (
     <>
       <h1>News Page</h1>
